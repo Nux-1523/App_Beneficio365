7c20902512801784e71d297a020de4df1e6c2116
Nux-1523 <A.VPP@hotmail.com>
2025-09-07 14:41:58 -0500

    Create Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,60 @@ import './App.css';
 import User from './components/getuser/User';
 import AddUser from './components/adduser/AddUser';
 import UpdateUser from './components/updateuser/UpdateUser';
+import Landing from './components/pages/Landing';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 
-function App() {
+// Permitira manejar los errores
+
+function ErrorPage({ error }) {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Oops! 😅</h1>
+      <p>{error?.statusText || error?.message || "Ha ocurrido un error"}</p>
+      <a href="/">Volver al inicio</a>
+    </div>
+  );
+}
+//Para las rutas que no encuentre
+function NotFound() {
+  return(
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404 Not Found 😬</h1>
+      <p>La ruta que buscas no existe.</p>
+      <a href="/">Volver al inicio</a>
+    </div>
+  );
+  }
+  // Defino las rutas
+
   const route = createBrowserRouter([
     {
-      path: "/",
-      element: <User />
+      path: "/",              
+      element: <Landing />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/users",
+      element: <User />,
+      errorElement: <ErrorPage />
     },
     {
       path: "/add",
-      element: <AddUser />
+      element: <AddUser />,
+      errorElement: <ErrorPage />
     },
     {
       path: "/update/:id",
-      element: <UpdateUser />
+      element: <UpdateUser />,
+      errorElement: <ErrorPage />
+    },
+    {
+    path: "*",
+    element: <NotFound />   // ✅ Aquí sí se usa NotFound
     }
   ])
 
-
+function App(){
   return (
     <div className="App">
       <header className="App-header">
